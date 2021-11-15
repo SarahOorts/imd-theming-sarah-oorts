1e2cbe93776291dbfad02163e48629845be8f4c8
SarahOorts <r0657122@student.thomasmore.be>
2021-11-15 16:39:40 +0100

    feat: add hamburger menu with toggle via drupal behavior. Create new nav.js file for drupal behavior. create new template for menu modification

---
+++ themes/sarah/js/nav.js
@@ -0,0 +1,43 @@
+/**
+ * @file
+ * Placeholder file for custom sub-theme behaviors.
+ *
+ */
+ (function ($, Drupal) {
+    'use strict';
+    
+  let hamburger = $(".hamburger-menu");
+  let hamburgeritem = $(".hamburger-menu-item");
+
+  console.log("test 1");
+
+     //Create behavior for navigation
+     Drupal.behaviors.navMenuBehavior = {
+      attach: function (context, settings) {
+        $('.hamburger-menu', context).once('navMenuBehavior').each(function () {
+          let open = false;
+          console.log("test 2");
+
+          hamburger.click(function() {
+              console.log(open);
+              if(open){
+                hamburgeritem.addClass("hidden");
+                $(".nav-menu").css("padding-bottom", "50px");                
+                $(".nav-menu").css("background-color", "rgb(0, 0, 0, 0.4");
+                console.log("test 3");
+                open = false;
+              } else {
+                hamburgeritem.removeClass("hidden");                
+                $(".nav-menu").css("padding-bottom", "230px");
+                $(".nav-menu").css("background-color", "rgb(0, 0, 0, 1");
+                console.log("test 4");
+                open = true;
+              }
+
+          });
+
+        });
+      }
+    };
+  
+  })(jQuery, Drupal);
