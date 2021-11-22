eaa6f4606010acc9cee30c91a5fe88b12f6e20fd
SarahOorts <r0657122@student.thomasmore.be>
2021-11-22 17:40:59 +0100

    feat: last styling for menu

--- themes/sarah/js/nav.js
+++ themes/sarah/js/nav.js
@@ -22,13 +22,13 @@
               console.log(open);
               if(open){
                 hamburgeritem.addClass("hidden");
-                $(".nav-menu").css("padding-bottom", "50px");                
+                $(".nav-menu").css("padding-bottom", "20px");                
                 $(".nav-menu").css("background-color", "rgb(0, 0, 0, 0.4");
                 console.log("test 3");
                 open = false;
               } else {
                 hamburgeritem.removeClass("hidden");                
-                $(".nav-menu").css("padding-bottom", "250px");
+                $(".nav-menu").css("padding-bottom", "230px");
                 $(".nav-menu").css("background-color", "rgb(0, 0, 0, 1");
                 console.log("test 4");
                 open = true;
